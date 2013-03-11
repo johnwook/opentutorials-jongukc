157436d263b61194ea76443f9ad746e3fb74ac9e
jongukc <silenttbfl@gmail.com>
2013-03-11 20:57:03 +0900

    change mysql password of index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 // 1. 데이터베이스 서버에 접속
-$link = mysql_connect('localhost', 'root', 'xlsk87');
+$link = mysql_connect('localhost', 'root', 'xlsk');
 if (!$link) {
 	die('Could not connect: ' . mysql_error());
 }
